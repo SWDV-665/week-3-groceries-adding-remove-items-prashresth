55d85dd1d61ba21887e6f526c9290e9e8fcd92df
PrashantShrestha <80365432+prashresth@users.noreply.github.com>
2021-07-09 17:24:29 -0500

    feat: adding providers

--- groceries/src/app/app.module.ts
+++ groceries/src/app/app.module.ts
@@ -10,6 +10,9 @@ import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { GroceriesServiceProvider } from '../providers/groceries-service/groceries-service';
+import { HttpClientModule } from '@angular/common/http';
+import { InputDialogServiceProvider } from '../providers/input-dialog-service/input-dialog-service';
 
 @NgModule({
   declarations: [
@@ -21,7 +24,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    HttpClientModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
@@ -34,7 +38,9 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    GroceriesServiceProvider,
+    InputDialogServiceProvider
   ]
 })
 export class AppModule {}
--- groceries/src/pages/home/home.ts
+++ groceries/src/pages/home/home.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import {ModalController, NavController} from 'ionic-angular';
 import { ToastController } from 'ionic-angular';
 import { AlertController } from 'ionic-angular';
-import { ItemSliding } from 'ionic-angular';
-import * as uuid from 'uuid';
+import { GroceriesServiceProvider } from "../../providers/groceries-service/groceries-service";
+import { InputDialogServiceProvider } from "../../providers/input-dialog-service/input-dialog-service";
 
 @Component({
   selector: 'page-home',
@@ -13,128 +13,29 @@ export class HomePage {
 
   title = 'Groceries List';
 
-  items = [
-    {id: uuid.v4(), name: 'Bread', quantity: 4, price: 2.49},
-    {id: uuid.v4(), name: 'Eggs', quantity: 2, price: 3.49},
-    {id: uuid.v4(), name: 'Cheese', quantity: 1, price: 4.99},
-    {id: uuid.v4(), name: 'Milk', quantity: 2, price: 1.99}
-  ];
-
   constructor(public navCtrl: NavController,
               public toastCtrl: ToastController,
-              public alertCtrl: AlertController) {
+              public alertCtrl: AlertController,
+              public groceryService: GroceriesServiceProvider,
+              public inputDialogService: InputDialogServiceProvider,
+              public modalController: ModalController) {
 
   }
 
-  async deleteItem(item, index) {
-    // another way to do it
-    // this.items = this.items.filter(i => i.id !== item.id)
-    this.items.splice(index, 1)
-    const toast = this.toastCtrl.create({
-      message: `Item: ${item.name} removed from the list.`,
-      duration: 3000,
-      cssClass: "toast-danger"
-    });
-    await toast.present();
+  loadItems() {
+    return this.groceryService.getAllItems();
   }
 
   async addItem() {
-    const prompt = this.alertCtrl.create({
-      title: 'Add Item',
-      message: "Fill out the form below to add a grocery item",
-      inputs: [
-        {
-          name: 'name',
-          placeholder: 'Name'
-        },
-        {
-          name: 'quantity',
-          placeholder: 'Quantity'
-        },
-        {
-          name: 'price',
-          placeholder: 'Price'
-        },
-      ],
-      buttons: [
-        {
-          text: 'Cancel',
-          handler: () => {
-            const toast = this.toastCtrl.create({
-              message: `It's ok, add an item some other time...`,
-              duration: 3000,
-              cssClass: 'toast-primary'
-            });
-            toast.present();
-          }
-        },
-        {
-          text: 'Add',
-          handler: item => {
-            item.id = uuid.v4()
-            this.items.push(item)
-            const toast = this.toastCtrl.create({
-              message: `Item: ${item.name} added to the list.`,
-              duration: 3000,
-              cssClass: 'toast-success'
-            });
-            toast.present();
-          }
-        }
-      ]
-    });
-    await prompt.present();
+    await this.inputDialogService.showPrompt();
   }
 
-  async editItem(item, index, slidingItem: ItemSliding) {
-    const prompt = this.alertCtrl.create({
-      title: 'Edit Item',
-      message: "Fill out the form below to edit the grocery item",
-      inputs: [
-        {
-          name: 'name',
-          value: `${item.name}`
-        },
-        {
-          name: 'quantity',
-          value: `${item.quantity}`
-        },
-        {
-          name: 'price',
-          value: `${item.price}`
-        },
-      ],
-      buttons: [
-        {
-          text: 'Cancel',
-          handler: () => {
-            slidingItem.close()
-            const toast = this.toastCtrl.create({
-              message: `It's ok, edit the item some other time...`,
-              duration: 3000,
-              cssClass: 'toast-primary'
-            });
-            toast.present();
-          }
-        },
-        {
-          text: 'Save',
-          handler: item => {
-            slidingItem.close()
-            let itemToEdit = this.items[index]
-            itemToEdit.name = item.name;
-            itemToEdit.quantity = item.quantity;
-            itemToEdit.price = item.price;
-            const toast = this.toastCtrl.create({
-              message: `Item updated successfully.`,
-              duration: 3000,
-              cssClass: 'toast-primary'
-            });
-            toast.present();
-          }
-        }
-      ]
-    });
-    await prompt.present();
+  async editItem(item, index, itemSliding) {
+    await this.inputDialogService.showPrompt(item, index, itemSliding);
+  }
+
+  async deleteItem(item, index) {
+    this.groceryService.deleteItem(index);
+    await this.inputDialogService.createDeleteToast(item);
   }
 }
